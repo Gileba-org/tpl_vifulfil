2de6ca9a7c73895aab9fe70dfa419de448ac7d12
Gijs Lamon <info@gileba.be>
2022-05-09 21:59:07 +0200

    Style offline page

--- src/index.php
+++ src/index.php
@@ -1,12 +1,16 @@
 <?php
 	defined('_JEXEC') or die('Restricted access');
+
+	$this->setGenerator('');
 ?>
 
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" >
    <head>
-		<jdoc:include type="head" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	   <jdoc:include type="metas" />
+	   <jdoc:include type="styles" />
+	   <jdoc:include type="scripts" />
 	</head>
 	<body>
 	</body>
