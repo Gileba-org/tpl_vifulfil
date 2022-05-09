9a39277ba19ab7a79f063b194b26f6efb94a98ef
Gijs Lamon <info@gileba.be>
2022-05-09 20:54:52 +0200

    Basic empty template

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+	defined('_JEXEC') or die('Restricted access');
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" >
+   <head>
+		<jdoc:include type="head" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+	</body>
+</html>
